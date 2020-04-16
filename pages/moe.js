facc94eb898cf09bd221b0c621d3b7707bfee12b
Mohamed Mohamed <mohamed.mohamed@thetrainline.com>
2020-04-16 15:52:59 +0100

    Moe

---
+++ pages/moe.js
@@ -0,0 +1,35 @@
+export default function Index() {
+  return (
+    <div class="wrapper">
+      <link rel="stylesheet" type="text/css" href="/moe.css" />
+      <section class="event">
+        <div class="cell well">
+          <div class="event-content">
+            <img
+              src="https://i.imgur.com/Sk7zz1D.jpg"
+              class="photoSwingReverse"
+            />
+            <img src="https://i.imgur.com/g6rZKMS.jpg" class="photoSwing" />
+          </div>
+        </div>
+      </section>
+      <marquee scrollamount="20">🥳🤩😎😘🤪😁👻</marquee>
+      <div class="cake">
+        <div class="plate"></div>
+        <div class="layer layer-1"></div>
+        <div class="layer layer-2"></div>
+        <div class="layer layer-3"></div>
+        <div class="layer layer-4"></div>
+        <div class="layer layer-top"></div>
+        <div class="layer layer-top-cream1"></div>
+        <div class="layer layer-top-cream2"></div>
+        <div class="layer candle"></div>
+      </div>
+      <div class="msg gradient-heading">
+        <p>Happy Birthday Jen!!!</p>
+        <p>Hope you have an amazing day and we'll celebrate big once we quarantine free</p>
+        <p> Lots of love from Fizzy and Moe</p>
+      </div>
+    </div>
+  );
+}
